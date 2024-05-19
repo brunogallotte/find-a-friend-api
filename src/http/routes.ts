320ffebfe3796991c247357781b38d7ddff6b089
Bruno Gallotte <brunogallotte1@gmail.com>
2024-05-19 16:08:51 -0300

    Feat: add refresh token strateg

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,12 @@ import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { addPet } from './controllers/add-pet'
 import { verifyJWT } from './middlewares/verify-jwt'
+import { refresh } from './controllers/refresh'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   /** Authorized **/
   app.post('/pets', { onRequest: [verifyJWT] }, addPet)
